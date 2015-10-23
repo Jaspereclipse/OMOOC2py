7966118a8693ff3e8d134f381e673235b607624f
Juanyan Li <juanyan.li@berkeley.edu>
2015-10-23 13:12:46 -0700

    Third commit: fix markdown display, again

--- _src/om2py0w/0wex1/Jaspers_Journal.md
+++ _src/om2py0w/0wex1/Jaspers_Journal.md
@@ -1,7 +1,13 @@
 
-            ##Fri Oct 23 13:04:34 2015            
-This is the my journal!
-            ##Fri Oct 23 13:04:52 2015            
-###Test some markdown syntax
-            ##Fri Oct 23 13:05:06 2015            
-Ready to upload!
+
+************##Fri Oct 23 13:11:34 2015************
+
+Try
+
+************##Fri Oct 23 13:11:39 2015************
+
+New things
+
+************##Fri Oct 23 13:11:45 2015************
+
+Ready to commit
--- _src/om2py0w/0wex1/main.py
+++ _src/om2py0w/0wex1/main.py
@@ -6,10 +6,7 @@ def main():
     dir = '/home/oski/OMOOC2py/_src/om2py0w/0wex1/Jaspers_Journal.md'
     f = open(dir,'a+')
     print f.read()
-    headertime = \
-    '''
-    {0:^50}
-    '''
+    headertime = "{0:*^50}"
     s = '##' + time.strftime("%c")
     now = headertime.format(s)
     Msg = ''
@@ -18,10 +15,10 @@ def main():
         if tmp == 'q':
             break
         else:
-            Msg += '\n\t' + tmp
+            Msg += '\n\n' + tmp
     if len(Msg) > 0:
-        f.write(now)
-    f.write(Msg)
+        f.write('\n\n' + now)
+        f.write(Msg)
     f.close()
 
 if __name__=="__main__":
