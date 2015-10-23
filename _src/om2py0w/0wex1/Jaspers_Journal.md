3a416079f46f1e0aeb89894c4c5ad753b4e6440e
Juanyan Li <juanyan.li@berkeley.edu>
2015-10-23 12:52:28 -0700

    First Commit: V-1.0 Simple Journal

---
+++ _src/om2py0w/0wex1/Jaspers_Journal.md
@@ -0,0 +1,7 @@
+
+                ##Fri Oct 23 12:50:06 2015            
+    
+	This is my first journal
+                ##Fri Oct 23 12:50:19 2015            
+    
+	Complete!
---
+++ _src/om2py0w/0wex1/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import time
+
+def main():
+    dir = '/home/oski/OMOOC2py/_src/om2py0w/0wex1/Jaspers_Journal.md'
+    f = open(dir,'a+')
+    print f.read()
+    headertime = \
+    '''
+    {0:^50}
+    '''
+    s = '##' + time.strftime("%c")
+    now = headertime.format(s)
+    Msg = ''
+    while 1:
+        tmp = raw_input()
+        if tmp == 'q':
+            break
+        else:
+            Msg += '\n\t' + tmp
+    if len(Msg) > 0:
+        f.write(now)
+    f.write(Msg)
+    f.close()
+
+if __name__=="__main__":
+    main()
